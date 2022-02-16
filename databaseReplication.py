ed075e75308df7b104af89e3045835758b9ae736
Sezer Burkaş <sezerburkas@gmail.com>
2022-02-17 00:31:34 +0300

    find exist tables and gather column information

--- databaseReplication.py
+++ databaseReplication.py
@@ -1,17 +1,49 @@
 import pyodbc 
+import json
 
-conn = pyodbc.connect('Driver={SQL Server};'
+conn_m = pyodbc.connect('Driver={SQL Server};'
                       'Server=STARFIGHTER;'
                       'Database=analizor;'
                       'UID=phpinterface;'
                       'PWD=php')
 
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM INFORMATION_SCHEMA.TABLES')
+conn_b = pyodbc.connect('Driver={SQL Server};'
+                      'Server=STARFIGHTER;'
+                      'Database=test_database;'
+                      'UID=phpinterface;'
+                      'PWD=php')
 
-tables = []
+cursor_m = conn_m.cursor()
+cursor_b = conn_b.cursor()
+cursor_m.execute('SELECT * FROM INFORMATION_SCHEMA.TABLES')
+cursor_b.execute('SELECT * FROM INFORMATION_SCHEMA.TABLES')
 
-for i in cursor:
-    tables.append(i[2])
+tables_main = []
+tables_backup = []
 
-print(tables)
+for i in cursor_m:
+    tables_main.append(i[2])
+
+for i in cursor_b:
+    tables_backup.append(i[2])
+
+col_names = []
+s=0
+mapping = [("[","("),("]",")"),('"',"")]
+
+
+for i in tables_main:
+    if(i in tables_backup):
+        cursor_m.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{}' ORDER BY ORDINAL_POSITION".format(i))
+        for k in cursor_m:
+            if(s == 0):
+                col = "({}".format(k[0])
+            else:
+                col += " ,{}".format(k[0])
+            s=s+1
+        col += ")"
+        print(col)
+
+
+
+
