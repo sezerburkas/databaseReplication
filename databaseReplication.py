6511140164214414318198672d0f55a758e8c39b
Sezer Burkaş <sezerburkas@gmail.com>
2022-02-17 01:00:44 +0300

    run sql commands

--- databaseReplication.py
+++ databaseReplication.py
@@ -20,6 +20,7 @@ cursor_b.execute('SELECT * FROM INFORMATION_SCHEMA.TABLES')
 
 tables_main = []
 tables_backup = []
+insert_queries = []
 
 for i in cursor_m:
     tables_main.append(i[2])
@@ -52,18 +53,18 @@ for i in tables_main:
         if(count1 > count2):
             count = count1 - count2
             cursor_m.execute("SELECT TOP {} * FROM {} ORDER BY {} DESC".format(count, i, first_table))
-            s=0
             for v in cursor_m:
+                s=0
                 for z in v:
                     if(s == 0):
-                        data = '("{}"'.format(z)
+                        data = "('{}'".format(z)
                     else:
-                        data += ' ,"{}"'.format(z)
+                        data += ", '{}'".format(z)
                     s=s+1
                 data += ")"
                 insert = "INSERT INTO {} {} VALUES {}".format(i, col, data)
-                print(insert)
-
+                cursor_b.execute(insert)
+            
 
 
 
